51f2bdb0059eef229a953259cb3bca9d32ff68d2
abiusx <git@abiusx.com>
2015-06-05 15:02:39 -0400

    init updated.  should be an object so that it can always retain global variables

--- main.php
+++ main.php
@@ -1,18 +1,13 @@
 <?php
 require_once __DIR__."/PHP-Parser/lib/bootstrap.php";
 use PhpParser\Node;
-class MyNodeVisitor extends  PhpParser\NodeVisitorAbstract
-{
-	public static $result=[];
-	public static $file;
-}
 
 class Emulator
 {	
 	static $infinite_loop=20; #1000000;
 	protected $last_node,$last_file;
 	public $output;
-	public $variables=[];
+	public $variables=[]; #TODO: make this a class, so that it can lookup magic variables, an retain them on push/pop
 	public $functions=[];
 	public $parser;
 	public $variable_stack=[];
@@ -377,14 +372,18 @@ class Emulator
 	function __construct()
 	{
 		$this->parser = new PhpParser\Parser(new PhpParser\Lexer);
-		$this->traverser     = new PhpParser\NodeTraverser;
-    	$this->traverser->addVisitor(new LiteralExplodeDetector);
+		// $this->traverser     = new PhpParser\NodeTraverser;
+    	// $this->traverser->addVisitor(new LiteralExplodeDetector);
     	$this->init();
 	}
 	function init()
 	{
-		$this->variables['_GET']=isset($_GET)?$_GET:array();
-		$this->variables['_POST']=isset($_POST)?$_POST:array();
+		foreach ($GLOBALS as $k=>$v)
+		{
+			if ($k=="GLOBALS") continue;
+			$this->variables[$k]=$v;
+		}
+		// $this->variables['_POST']=isset($_POST)?$_POST:array();
 
 	}
 	function start($file)
@@ -537,93 +536,9 @@ class Emulator
 
 
 
-
-class LiteralExplodeDetector extends MyNodeVisitor
-{
-    public function leaveNode_(Node $node) {
-        if ($node instanceof Node\Scalar\String_) {
-            $node->value = 'foo';
-        }
-        if ($node instanceof Node\Name) {
-            return new Node\Name($node->toString('_'));
-        } elseif ($node instanceof Stmt\Class_
-                  || $node instanceof Stmt\Interface_
-                  || $node instanceof Stmt\Function_) {
-            $node->name = $node->namespacedName->toString('_');
-        } elseif ($node instanceof Stmt\Const_) {
-            foreach ($node->consts as $const) {
-                $const->name = $const->namespacedName->toString('_');
-            }
-        } elseif ($node instanceof Stmt\Namespace_) {
-            // returning an array merges is into the parent array
-            return $node->stmts;
-        } elseif ($node instanceof Stmt\Use_) {
-            // returning false removed the node altogether
-            return false;
-        }
-    }
-    public function beforeTraverse(array $nodes) {}
-	public function enterNode(PhpParser\Node $node){}
-	public function leaveNode(PhpParser\Node $node)
-	{
-		if ($node instanceof Node\Expr\FuncCall)
-		{
-			if ($node->name=="explode" and $node->args[0]->value instanceof Node\Scalar\String_)
-			{
-				// print_r($node);
-				self::$result[]=array(
-					"function"=>$node->name->__toString(),
-					"value"=>$node->args[0]->value->value,
-					"line"=>$node->args[0]->value->getLine(),
-					"file"=>self::$file
-					);
-				// print_r($this->result);
-			}
-		}
-	}
-	public function afterTraverse(array $nodes){}
-}
-
-function analyze($dir,$visitor)
-{
-	ini_set("memory_limit",-1);
-	$files=new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-	$parser = new PhpParser\Parser(new PhpParser\Lexer);
-    $traverser     = new PhpParser\NodeTraverser;
-    $traverser->addVisitor(new $visitor);
-	$str="Parsing all files in {$dir}";
-	$count=strlen($str);
-	echo $str;
-	$statements=[];
-	foreach ($files as $k=>$file)
-	{
-		if ($file=="." or $file=="..") continue;
-		$fileinfo = pathinfo($file);
-		if (isset($fileinfo['extension']) and $fileinfo['extension']!=="php") continue;
-		echo ".";
-		$count++;
-		if ($count%80==0) echo PHP_EOL;
-		$code=file_get_contents($file);	
-		try {
-			$visitor::$file=$file->__toString();
-		    $statements[$k] = $parser->parse($code);
-		    $statements[$k] = $traverser->traverse($statements[$k]);
-
-		    // $statements is an array of statement nodes
-		} catch (PhpParser\Error $e) {
-		    echo 'Parse Error: ', $e->getMessage()," in file {$file}",PHP_EOL;
-		}
-		// print_r($statements);
-	}
-	echo PHP_EOL;
-	printf("Memory Usage: %.2fKB\n",memory_get_usage()/1024);
-	printf("Parsed a total of %d files\n",$count);
-	// echo memory_get_usage()/1024,"KB,",memory_get_peak_usage()/1024,"KB",PHP_EOL;
-	return $statements;
-}
 $_GET['url']='http://abiusx.com/blog/wp-content/themes/nano2/images/banner.jpg';
 $x=new Emulator;
-$x->start("sample3.php");
+$x->start("sample.php");
 var_dump($x->output);
 echo PHP_EOL,"### Variables ###",PHP_EOL;
 var_dump($x->variables);
