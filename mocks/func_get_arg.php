2ff239f24df4724cfe168a8123ab74865e396ab0
Abbas N <abiusx@AbiusX-MBP.local>
2016-01-18 14:39:38 -0500

    func_get_arg* functions were still using the old backtrace standard for checks, and were giving wrong errors.

--- mocks/func_get_arg.php
+++ mocks/func_get_arg.php
@@ -2,12 +2,12 @@
 function func_get_arg_mock(Emulator $emul,$arg_num)
 {
 
-	if (count($emul->trace)==0 or !isset(end($emul->trace)->args))
+	if (count($emul->trace)==0 or !isset($emul->trace[count($emul->trace)-2]->args))
 	{
 		$emul->warning("func_get_arg():  Called from the global scope - no function context");
 		return false;
 	}
-	elseif (count(end($emul->trace)->args)<=$arg_num)
+	elseif (count($emul->trace[count($emul->trace)-2]->args)<=$arg_num)
 	{
 		$emul->warning("func_get_arg():  Argument {$arg_num} not passed to function");
 		return false;
--- mocks/func_get_args.php
+++ mocks/func_get_args.php
@@ -1,7 +1,7 @@
 <?php
 function func_get_args_mock(Emulator $emul)
 {
-	if (count($emul->trace)==0 or !isset(end($emul->trace)->args))
+	if (count($emul->trace)==0 or !isset($emul->trace[count($emul->trace)-2]->args))
 	{
 		$emul->warning("func_get_args():  Called from the global scope - no function context");
 		return false;
