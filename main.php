92644604fbcdc0c4820ac3b441e299d5d5a80af8
abiusx <git@abiusx.com>
2016-01-25 18:31:01 -0500

    CLI support. no more hardcoding files.

--- main.php
+++ main.php
@@ -1,28 +1,34 @@
 <?php
 if (!function_exists("apache_getenv"))
 {
-	
 	function apache_getenv()
 	{
 		return "";
 	}
 }
 
-ini_set("memory_limit",-1);
-require_once "oo.php";
 
 
-
+$usage="Usage: php main.php -f file.php [-v verbosity -o]\n";
 if (isset($argc) and realpath($argv[0])==__FILE__)
 {
+	$options=getopt("f:v:o");
+	if (!isset($options['f']))
+		die($usage);
+	
+	ini_set("memory_limit",-1);
+	require_once "oo.php";
 	$x=new OOEmulator;
-	$x->direct_output=false;
-	$x->verbose=10;
-	$entry_file="samples/sample-mocks.php";
-	// $entry_file="samples/sample-class-exists.php";
+	$x->direct_output=isset($options['o']);
+	if (isset($options['v'])) $x->verbose=$options['v'];
+	$entry_file=$options['f'];
+	// $entry_file="samples/sample-forward-static.php";
 	// $entry_file="wordpress/wp-admin/install.php";
 	// $entry_file="wordpress/index.php";
 
 	$x->start($entry_file);
-	file_put_contents("output.txt",$x->output);
+	if (!isset($options['o'])) 
+		file_put_contents("output.txt",$x->output);
 }
+else
+	die($usage);
