df4a9c68cee2c92b7496fc77dd9a5f29935fd306
abiusx <git@abiusx.com>
2016-02-29 16:34:10 -0500

    auto tester

--- test.php
+++ test.php
@@ -1,17 +1,62 @@
 <?php
-
+if (count($argv)<=1)
+	die("Usage: test.php tests/*.php\n");
 echo "This tool checks whether a PHP script runned by the emulator matches the same script runned by native PHP.",PHP_EOL;
-require_once "main.php";
+// require_once "main.php";
 
-$file=$options['f'];
-$res=shell_exec("php '{$file}'");
-
-echo str_repeat("-", 80),PHP_EOL;
-if ($res===$x->output)
-	echo "Compatible!",PHP_EOL;
-else
+array_shift($argv); //test.php
+if ($argv[1]=='--diff')
 {
-	echo "Incompatible:",PHP_EOL;
-	file_put_contents("actual.txt", $res);
-	echo shell_exec("diff --text actual.txt output.txt");
+	$diff=true;
+	array_shift($argv); 
 }
+do_test($argv);
+
+
+function do_test($files)
+{
+	foreach ($files as $file)
+	{
+		global $diff;
+
+		if (is_dir($file))
+		{
+
+			$str=" Directory {$file} ";
+			$x=80-strlen($str);
+			echo str_repeat("-", $x/2),$str,str_repeat("-",$x/2),PHP_EOL;
+			do_test($file);
+		}
+		$x=80-strlen($file)-4;
+		
+		if ($diff) echo str_repeat("-",$x/2), " ";
+		echo $file;
+		fflush(STDOUT);
+		$res1=shell_exec("php '{$file}' 2>&1");
+		shell_exec("php main.php -f '{$file}'  -o output.txt 2>&1");
+		$res2=file_get_contents("output.txt");
+		if ($res1===$res2)
+		{
+
+			echo " \033[32m ",'✓ ',"\033[0m";
+			if ($diff) echo str_repeat("-",$x/2),PHP_EOL;
+		}
+		else
+		{
+			echo " \e[31m X ","\033[0m";
+			if ($diff)
+			{
+				echo str_repeat("-",$x/2),PHP_EOL;
+				file_put_contents("1.txt", $res1);
+				file_put_contents("2.txt", $res2);
+				echo shell_exec("diff --text 1.txt 2.txt");
+			}
+		}
+		if (!$diff) echo PHP_EOL;
+		fflush(STDOUT);
+	}
+
+}
+
+@unlink("1.txt");
+@unlink("2.txt");
