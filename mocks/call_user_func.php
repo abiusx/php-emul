5faebbd7b01f0c92af2509488c6316e3a7af44f5
abiusx <git@abiusx.com>
2016-03-02 18:29:42 -0500

    so did for call_user_func. much simplergit add -u

--- mocks/call_user_func.php
+++ mocks/call_user_func.php
@@ -4,18 +4,6 @@ function call_user_func_mock(Emulator $emul,  $callback)
 {
 	$args=func_get_args();
 	array_shift($args); //$emul
-	### DO NOT CHANGE ABOVE THIS LINE ###
-	
-	$args[0]=function() 
-			
-
-	### DO NOT CHANGE BELOW THIS LINE ###
-			use ($emul,$callback)  
-			{
-				// $argz=func_get_args();
-				$argz=debug_backtrace()[0]['args']; //byref hack
-				return $emul->call_function($callback,$argz);
-			};
-	$real_function=substr(__FUNCTION__,0,-strlen("_mock"));
-	return call_user_func_array($real_function, $args);
+	array_shift($args); //$callback
+	return $emul->call_function($callback, $args);
 }
--- mocks/call_user_func_array.php
+++ mocks/call_user_func_array.php
@@ -3,12 +3,4 @@
 function call_user_func_array_mock(Emulator $emul,  $callback , array $param_arr )
 {
 	return $emul->call_function($callback,$param_arr);
-	// $callback=function() 
-	// 		use ($emul,$callback)  
-	// 		{
-	// 			// $argz=func_get_args();
-	// 			$argz=debug_backtrace()[0]['args']; //byref hack
-	// 			return $emul->call_function($callback,$argz);
-	// 		};
-	// return call_user_func_array($callback, $param_arr);
 }
