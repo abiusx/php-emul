4cdecfab10ab20524194c2512099c6e5a9c629f4
abiusx <git@abiusx.com>
2016-03-02 17:34:09 -0500

    the issue is not with str/array refs, refs are not working generally. gotta investigate.

--- samples/sample-reference.php
+++ samples/sample-reference.php
@@ -1,12 +1,24 @@
 <?php
+$x=5;
+$z=&$x;
+$z++;
+var_dump($x);
+exit(0);
+
 $str="abc";
 
 $str_ref_arr=array(&$str,2,3);
+$just_ref=&$str;
+
 
 $str_ref_arr[0].="xyz";
 var_dump($str_ref_arr);
 var_dump($str);
 
+$just_ref.="hello";
+
+var_dump($just_ref);
+var_dump($str);
 exit(0);
 
 $a=$b=5;
