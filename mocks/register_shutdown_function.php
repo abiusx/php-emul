b9175c520d2c856f5a18daf8a1ab3429dce93a5e
abiusx <git@abiusx.com>
2016-04-12 10:50:22 -0400

    verbose output for register shutdown function

--- mocks/register_shutdown_function.php
+++ mocks/register_shutdown_function.php
@@ -5,5 +5,9 @@ function register_shutdown_function_mock(Emulator $emul, $callback,$parameter=nu
 	$args=func_get_args();
 	array_shift($args); //emulator
 	array_shift($args); //$callback
+	$name=$callback;
+	if(is_array($name))
+		$name=implode("::",$name);
+	$emul->verbose("Registering shutdown function '{$name}'...\n",4);
 	$emul->shutdown_functions[]=(object)array("callback"=>$callback,"args"=>$args);
 }
