10bb8401399247ce0530effb85345f03b6631fc7
abiusx <git@abiusx.com>
2016-06-08 10:43:55 -0400

    unstable: added support for encapsed string names

---
+++ samples/sample-namespace.php
@@ -0,0 +1,13 @@
+<?php
+// namespace NS\NS2 {
+	$x=5;
+	${"hello_{$x}"}=2; //regular parts name
+	var_dump($hello_5);
+	// class X {};
+// }
+// namespace OUT {
+	// $x=new \NS\NS2\X;
+	// var_dump($x);
+// }
+
+
