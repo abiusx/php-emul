26aeb4dfa0ebb656650027ecb352587a3f71a94e
abiusx <git@abiusx.com>
2016-08-01 11:55:16 -0400

    constant was not mocked! it crashed many execution

---
+++ mocks/constant.php
@@ -0,0 +1,8 @@
+<?php
+function constant_mock($emul,$constant_name)
+{
+	if (isset($emul->constants[$constant_name]))
+		return $emul->constants[$constant_name];
+	else
+		return constant($constant_name);
+}
