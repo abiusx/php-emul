0e5c6ca46fa05d760ab822698f36f02b6ff70d62
abiusx <git@abiusx.com>
2016-08-14 14:45:30 -0400

    callback is not necessarily string, fixed

--- mocks/register_shutdown_function.php
+++ mocks/register_shutdown_function.php
@@ -7,7 +7,12 @@ function register_shutdown_function_mock(Emulator $emul, $callback,$parameter=nu
 	array_shift($args); //$callback
 	$name=$callback;
 	if(is_array($name))
+	if (is_string($name[0]))
 		$name=implode("::",$name);
+	elseif($name[0] instanceof EmulatorObject)
+		$name=$name[0]->classname."->".$name[1];
+	else
+		$name="Unknown::{$name[1]}";
 	$emul->verbose("Registering shutdown function '{$name}'...\n",4);
 	$emul->shutdown_functions[]=(object)array("callback"=>$callback,"args"=>$args);
 }
